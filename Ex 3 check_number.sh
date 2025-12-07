3b7f270553bc440cd3f2055a7f7bbb201dc463d7
serchechikhin-hub <ser.chechikhin@gmail.com>
2025-12-08 02:11:02 +0300

    Create Ex 3 check_number.sh

---
+++ Ex 3 check_number.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Введите число: " number
+
+if (( $(echo "$number > 0" | bc -l) )); then
+    echo "Введено положительное число"
+elif (( $(echo "$number < 0" | bc -l) )); then
+    echo "Введено отрицательное число"
+else
+    echo "Введенное число - ноль"
+fi
