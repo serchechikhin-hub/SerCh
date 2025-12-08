46333c9584e80ae32103f3684e6f8ef784df8315
serchechikhin-hub <ser.chechikhin@gmail.com>
2025-12-08 22:36:48 +0300

    Update Ex 7 read_file.sh

--- Ex 7 read_file.sh
+++ Ex 7 read_file.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+read -p "Введите имя файла: " filename
+echo "нетология" > "$filename"
+echo "вшэ" >> "$filename"
+echo "обучение" >> "$filename"
+echo "devsecops" >> "$filename"
+echo "Файл $filename создан"
 
+echo "Содержимое файла '$filename':"
+echo "----------------------------------------"
+
+# Читаем файл построчно и выводим на экран
+line_number=1
+while IFS= read -r line; do
+    echo "Строка $line_number: $line"
+    ((line_number++))
+done < "$filename"
+
+echo "----------------------------------------"
+echo "Количество строк: $((line_number-1))"
