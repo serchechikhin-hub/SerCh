3548e2f7c3a497e3968cb9a382446ff837e14b70
serchechikhin-hub <ser.chechikhin@gmail.com>
2025-12-08 23:35:41 +0300

    Update Ex 8  replace_text.sh

--- Ex 8  replace_text.sh
+++ Ex 8  replace_text.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+read -p "Введите имя файла: " filename
+echo "нетология-bash" > "$filename"
+echo "нетология-вшэ" >> "$filename"
+echo "нетология-обучение" >> "$filename"
+echo "нетология-devsecops" >> "$filename"
+echo "Файл $filename создан"
 
+read -p "Введите слово, которое хотите изменить: " word1
+read -p "Введите слово, на которое хотите заменить: " word2
+
+# Заменяем слова
+sed -i "s/$word1/$word2/g" "$filename"
+
+echo "Содержимое файла '$filename':"
+echo "----------------------------------------"
+
+# Читаем файл построчно и выводим на экран
+line_number=1
+while IFS= read -r line; do
+    echo "Строка $line_number: $line"
+    ((line_number++))
+done < "$filename"
